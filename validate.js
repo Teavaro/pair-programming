812ebf2453c972a5c33f06ab54c1e0e8c364b59b
tapia <alexchirolde@gmail.com>
2024-02-13 12:33:57 +0100

    first commit

--- validate.js
+++ validate.js
@@ -1,5 +1,18 @@
 function validate(validationObject, data) {
   /* Implementation intructions provided during the pair programming */
+
+  let dataArray = Object.entries(data);
+
+  let validated = true;
+
+  dataArray.forEach(el => {
+    if (!(el[0] in validationObject) || typeof el[1] !== validationObject[el[0]]) {
+      validated = false;
+    }
+  });
+
+  return validated;
+
 }
 
 module.exports = validate;
